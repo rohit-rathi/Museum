2a97c10f6e8d8672e9ff3fe331a2402b6556b2de
robrocop <rohit313.93@gmail.com>
2017-04-26 19:58:20 -0400

    issue with gvr reticle

---
+++ Assets/Display room/Scripts/PlayVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+	private AudioSource audio;
+	private bool isPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		audio = GetComponent<AudioSource> ();
+		audio.clip = movie.audioClip;
+		//movie.Play ();
+		//audio.Play ();
+
+	}
+
+	public void changeStatusOfMovie ()
+	{
+		if (isPlaying) {
+			movie.Pause ();
+			audio.Pause ();
+			Debug.Log ("pausing");
+		} 
+		else {
+			movie.Play ();
+			audio.Pause ();
+			Debug.Log ("playing");
+		}
+		isPlaying = !isPlaying;
+		
+	}
+}
